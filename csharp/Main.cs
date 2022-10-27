fb257b8908703244c27c57a6eb4f27adfdd7de7d
Josef Reichelt <pepareichelt@gmail.com>
2022-10-27 21:12:20 +0200

    Cors

--- csharp/Main.cs
+++ csharp/Main.cs
@@ -5,14 +5,22 @@ namespace hellocrud
         public const string BASE_URL = "/api/v1/";
         public const string UNITS_URL = BASE_URL + "units/";
 
+        public const string DB_URL = "Data Source=../database.sqlite";
+
     }
     class HelloCrud
     {
         static void Main(string[] args)
         {
-
+            var AllowedOrigins = "allowTheseOrigins";
             var builder = WebApplication.CreateBuilder(args);
-
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(name: AllowedOrigins, policy =>
+                {
+                    policy.WithOrigins("http://127.0.0.1:1337", "*").SetIsOriginAllowedToAllowWildcardSubdomains().AllowAnyHeader();
+                });
+            });
             // Add services to the container.
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -21,7 +29,6 @@ namespace hellocrud
 
             var app = builder.Build();
             app.UseHttpLogging();
-
             // app.MapControllerRoute("unittypes","{controller=UnitTypes}/{action=Index}");
 
             // Configure the HTTP request pipeline.
@@ -30,13 +37,19 @@ namespace hellocrud
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+            else
+            {
+                app.UseHttpsRedirection();
 
-            app.UseHttpsRedirection();
+            }
+
+            app.UseCors(AllowedOrigins);
 
             app.UseAuthorization();
 
             app.MapControllers();
 
+            Console.WriteLine("HelloCrud is running");
             app.Run();
         }
     }
