775971f352c4b5a1717298e8384f5a0fb33ea4eb
Josef Reichelt <pepareichelt@gmail.com>
2022-10-28 15:32:22 +0200

    Remove rust

--- rust/src/controllers/unit_types_controller.rs
+++
@@ -1,9 +0,0 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use serde::{Deserialize, Serialize};
-
-#[get("/api/v1/units/types")]
-pub async fn get_unit_types() -> impl Responder {
-    println!("GETTING UNITS");
-    crate::models::unit_types_model::get_unit_types_from_db();
-    return "Hi there";
-}
--- rust/src/models/unit_types_model.rs
+++
@@ -1,3 +0,0 @@
-pub fn get_unit_types_from_db(){
-    println!("GETTING UNITS FROM DB!!!");
-}
